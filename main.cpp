f5de8bc28776f97044f70a75c7699a93de45b308
Herp <160724713+FarisKurb@users.noreply.github.com>
2025-11-30 19:55:45 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <cstdlib>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+std::mutex print_lock;
+std::mutex station_lock;
+
+struct Train {
+    int id;
+    vector<string> route;
+    int index;
+    bool active;
+};
+
+int rnd(int a, int b) {
+    return a + (std::rand() % (b - a + 1));
+}
+
+void worker(Train* t)
+{
+    while (t->active)
+    {
+        string station = t->route[t->index];
+
+        {
+            std::lock_guard<std::mutex> lk(print_lock);
+            cout << "Train " << t->id << " MOVING TO " << station << endl;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(rnd(1000, 2000)));
+
+        station_lock.lock();
+
+        {
+            std::lock_guard<std::mutex> lk(print_lock);
+            cout << "Train " << t->id << " ARRIVED at " << station << endl;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(rnd(1000, 2000)));
+
+        {
+            std::lock_guard<std::mutex> lk(print_lock);
+            cout << "Train " << t->id << " LEAVING " << station << endl;
+        }
+
+        station_lock.unlock();
+
+        t->index = (t->index + 1) % t->route.size();
+    }
+
+    std::lock_guard<std::mutex> lk(print_lock);
+    cout << "Train " << t->id << " STOPPED" << endl;
+}
+
+int main() {
+    std::srand((unsigned)std::time(nullptr));
+
+    vector<string> route = { "Dushanbe", "Gissar", "Regar", "Pakhtaabad" };
+
+    vector<Train*> trains;
+    vector<std::thread> threads;
+
+    for (int i = 1; i <= 8; ++i) {
+        Train* t = new Train();
+        t->id = i;
+        t->route = route;
+        t->index = 0;
+        t->active = true;
+        trains.push_back(t);
+    }
+
+    for (auto* t : trains)
+        threads.emplace_back(worker, t);
+
+    cout << "PRESS ENTER TO STOP..." << endl;
+    std::cin.get();
+
+    for (auto* t : trains) t->active = false;
+
+    for (auto& th : threads)
+        if (th.joinable()) th.join();
+
+    cout << "Simulation finished!" << endl;
+
+    for (auto* t : trains) delete t;
+}
